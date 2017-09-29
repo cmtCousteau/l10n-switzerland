7fe05188c56897b4ad2120ff6f3578cfccef6e8a
marco.mon@windowslive.com <marco.mon@windowslive.com>
2017-09-29 13:33:45 +0200

    CO-903 Added new fonctionality for import pain002 files

---
+++ l10n_ch_fds_upload_sepa/wizards/fds_files_import_tobankstatments_wizard.py
@@ -0,0 +1,34 @@
+from odoo import models, api
+
+import base64
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class FdsFilesImportToBankStatementsWizardPain000(models.TransientModel):
+    _inherit = 'fds.files.import.tobankstatments.wizard'
+
+    @api.multi
+    def _import2bankStatements(self, fds_files_ids):
+        account_pain000 = self.env['account.pain000.parser']
+        for fds_file in fds_files_ids:
+            for pf_file in fds_file:
+                values = {'data_file': pf_file.data}
+                bs_import_obj = self.env['account.bank.statement.import']
+                bank_wiz_imp = bs_import_obj.create(values)
+
+                decoded_file = base64.b64decode(bank_wiz_imp.data_file)
+                if account_pain000.parse(decoded_file):
+                    pf_file.write({
+                        'state': 'done',
+                        'data': None,
+                     })
+
+                    _logger.info("[OK] import file '%s' to bank Statements",(pf_file.filename))
+                else:
+                    return super(
+                        FdsFilesImportToBankStatementsWizardPain000, self)._import2bankStatements(fds_files_ids)
+
+        self.msg_file_imported += fds_file.filename + "; "
+        return True
---
+++ l10n_ch_import_pain000/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Import pain001',
+    'version': '10.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Monzione Marco',
+    'website': '...',
+    'category': 'Banking addons',
+    'depends': [
+        'account',
+    ],
+}
