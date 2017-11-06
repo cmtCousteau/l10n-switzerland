a9d61dee0daf0e16a1eda2023c8fb6ce2d8de6c0
CmtCousteau <marco.mon@windowslive.com>
2017-11-06 08:57:23 +0100

    CO-1199 Implement payment return and auto reconciliation

--- l10n_ch_import_camt054/models/__init__.py
+++ l10n_ch_import_camt054/models/__init__.py
@@ -3,3 +3,4 @@ from . import account_bank_statement_line
 from . import account_move_line
 from . import fds_postfinance_file_camt
 from . import custom_import_stmt
+from . import account_bank_statement
