352d9ffccaae57f1b132a8c1556882fc0a442098
CmtCousteau <marco.mon@windowslive.com>
2017-11-24 10:58:51 +0100

    CO-1288 - Import of pain.002 now work with multiple invoices

--- l10n_ch_import_pain000/models/pain000_parser.py
+++ l10n_ch_import_pain000/models/pain000_parser.py
@@ -6,7 +6,8 @@ from lxml import etree
 class Pain000Parser(models.AbstractModel):
     _name = 'account.pain000.parser'
 
-    def parse(self, data):
+    def _parse_detail(self, result, node, ns):
+
         payment_order_obj = self.env['account.payment.order']
         payment_line_obj = self.env['account.payment.line']
         bank_payment_line_obj = self.env['bank.payment.line']
@@ -14,63 +15,81 @@ class Pain000Parser(models.AbstractModel):
             'account.bank.statement.import.camt.parser']
         undo_payment_line_obj = self.env['account.undo.payment_line']
 
+        account_bank_parser_obj.add_value_from_node(
+            ns, node, './ns:OrgnlEndToEndId', result,
+            'orgnl_end_to_end_id')
+
+        account_bank_parser_obj.add_value_from_node(
+            ns, node, './ns:TxSts',
+            result, 'tx_sts')
+
+        account_bank_parser_obj.add_value_from_node(
+            ns, node, './ns:StsRsnInf/ns:Rsn/ns:Cd', result,
+            'full_reconcile_id')
+
+        account_bank_parser_obj.add_value_from_node(
+            ns, node, './ns:StsRsnInf/ns:Rsn/ns:Cd', result,
+            'full_reconcile_id')
+
+        account_bank_parser_obj.add_value_from_node(
+            ns, node, './ns:StsRsnInf/ns:AddtlInf', result,
+            'add_tl_inf')
+
+        if 'orgnl_end_to_end_id' in result:
+            bank_payment_line = bank_payment_line_obj.search(
+                [('name', '=', result['orgnl_end_to_end_id'])])
+
+            if not bank_payment_line:
+                return
+
+            payment_line = payment_line_obj.search([(
+                'bank_line_id', '=', bank_payment_line.id)])
+
+            if not payment_line:
+                return
+
+            payment_order_obj = payment_order_obj.search(
+                [('name', '=', result['orgnl_msg_id'])])
+
+            if 'tx_sts' in result and result['tx_sts'] == 'RJCT':
+                    undo_payment_line_obj.undo_payment_line(
+                        payment_order_obj,
+                        payment_line,
+                        result)
+
+    def parse(self, data):
+        payment_order_obj = self.env['account.payment.order']
+        account_bank_parser_obj = self.env[
+            'account.bank.statement.import.camt.parser']
+
         root = etree.fromstring(data, parser=etree.XMLParser(recover=True))
         if root is not None:
             ns = root.tag[1:root.tag.index("}")]
 
             result = dict()
 
+            details_nodes = root.xpath(
+                './ns:CstmrPmtStsRpt/ns:OrgnlPmtInfAndSts/ns:TxInfAndSts',
+                namespaces={'ns': ns})
+
             account_bank_parser_obj.add_value_from_node(
                 ns, root, './ns:CstmrPmtStsRpt/ns:OrgnlGrpInfAndSts/ns'
                           ':OrgnlMsgId', result, 'orgnl_msg_id')
+
             account_bank_parser_obj.add_value_from_node(
                 ns, root, './ns:CstmrPmtStsRpt/ns:OrgnlGrpInfAndSts/ns'
                           ':OrgnlMsgNmId', result, 'orgnl_msg_nm_Id')
-            account_bank_parser_obj.add_value_from_node(
-                ns, root, './ns:CstmrPmtStsRpt/ns:OrgnlPmtInfAndSts/ns'
-                          ':TxInfAndSts/ns:OrgnlEndToEndId', result,
-                'orgnl_end_to_end_id')
-
-            account_bank_parser_obj.add_value_from_node(
-                ns, root, './ns:CstmrPmtStsRpt'
-                          '/ns:OrgnlPmtInfAndSts/ns:TxInfAndSts/ns:TxSts',
-                result, 'tx_sts')
-
-            account_bank_parser_obj.add_value_from_node(
-                ns, root, './ns:CstmrPmtStsRpt/ns:OrgnlPmtInfAndSts/ns'
-                          ':TxInfAndSts/ns:StsRsnInf/ns:Rsn/ns:Cd', result,
-                'full_reconcile_id')
-            account_bank_parser_obj.add_value_from_node(
-                ns, root,
-                './ns:CstmrPmtStsRpt/ns:OrgnlPmtInfAndSts/'
-                'ns:StsRsnInf/ns:Rsn/ns:Cd',
-                result,
-                'full_reconcile_id')
-
-            account_bank_parser_obj.add_value_from_node(
-                ns, root, './ns:CstmrPmtStsRpt/ns:OrgnlPmtInfAndSts/ns'
-                          ':TxInfAndSts/ns:StsRsnInf/ns:AddtlInf', result,
-                'add_tl_inf')
 
             if len(result) > 1 and 'orgnl_msg_nm_Id' in result:
                 if 'pain.001' not in result['orgnl_msg_nm_Id'] or \
                    'pain.008' not in result['orgnl_msg_nm_Id']:
 
-                    if 'orgnl_end_to_end_id' in result:
-                        bank_payment_line = bank_payment_line_obj.search(
-                            [('name', '=', result['orgnl_end_to_end_id'])])
+                    payment_order_obj = payment_order_obj.search(
+                        [('name', '=', result['orgnl_msg_id'])])
 
-                        payment_line = payment_line_obj.search([(
-                            'bank_line_id', '=', bank_payment_line.id)])
+                    for node in details_nodes:
+                        self._parse_detail(result, node, ns)
 
-                        payment_order_obj = payment_order_obj.search(
-                            [('id', '=', bank_payment_line.order_id.id)])
-
-                    if 'tx_sts' in result:
-                        if result['tx_sts'] == 'RJCT':
-                            undo_payment_line_obj.undo_payment_line(
-                                                payment_order_obj,
-                                                payment_line,
-                                                result)
                     return True, payment_order_obj
         return False, None
+
